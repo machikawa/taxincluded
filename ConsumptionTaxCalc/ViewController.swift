78883d2925b90384558072319fda14f0c3004dcf
Townriver <a1938hm@aiit.ac.jp>
2019-08-17 10:40:18 +0900

    Initial Commit

---
+++ ConsumptionTaxCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConsumptionTaxCalc
+//
+//  Created by たうんりばー on 2019/08/17.
+//  Copyright © 2019 hidemasa,machikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
